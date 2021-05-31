8ea4dc339b85019ae5bf6ae24e652cd5ef5733ef
co1248 <todaynownownow@gmail.com>
2021-05-31 11:59:09 +0900

    test

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+                  System.out.println("Hello world!");
+                  //한줄주석
+                  /* 여러줄주석 */
+	}
+
+}
